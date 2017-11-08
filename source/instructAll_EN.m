46c59092ae045486494e18e232ce92c11c2d1b2e
spooseh <spooseh@gmail.com>
2017-11-08 14:38:09 +0100

    Update instructAll_EN.m

--- source/instructAll_EN.m
+++ source/instructAll_EN.m
@@ -1,5 +1,5 @@
 function aborted = instructAll_EN(auxVars,N)
-
+% Instructions for all the tasks
 wd       = auxVars.wd;
 txtCol = auxVars.txtCol;
 i = 0; 
@@ -7,12 +7,12 @@ func{1} = [];
 yposm='center';
 i = i+1; 
 	ypos{i} = yposm;
-    tx{i} = ['Die Aufgabe kann nun beginnen. Sie können mit der '...
-             'rechten und linken Pfeiltaste vor- und zurück blättern.'];
+    tx{i} = ['Die Aufgabe kann nun beginnen. Sie kÃ¶nnen mit der '...
+             'rechten und linken Pfeiltaste vor- und zurÃ¼ck blÃ¤ttern.'];
     func{i} = 'getRarrow';
 i = i+1; 
 	ypos{i} = yposm;
-	tx{i} = ['Sie führen ' num2str(N) ' Test(s) zu Ihrem '...
+	tx{i} = ['Sie fÃ¼hren ' num2str(N) ' Test(s) zu Ihrem '...
              'Entscheidungsverhalten durch.'];     
     func{i} = 'getLRarrow';  
 
