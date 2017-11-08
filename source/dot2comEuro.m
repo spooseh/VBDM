bc84af3423014931a7e1e580c33fab7c4bd171f2
spooseh <spooseh@gmail.com>
2017-11-08 14:35:43 +0100

    Update dot2comEuro.m

--- source/dot2comEuro.m
+++ source/dot2comEuro.m
@@ -1,4 +1,4 @@
 function txt = dot2comEuro(rew)
-
-txt = sprintf('%d,%02d €',floor(rew),int64(mod(rew*100,100)));
+% Separates the cents with a comma
+txt = sprintf('%d,%02d â‚¬',floor(rew),int64(mod(rew*100,100)));
 txt = double(txt);
